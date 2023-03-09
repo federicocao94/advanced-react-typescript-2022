67fd269a112f1f7a95c4611b7392367454367e4a
Federico Cao <fcao@euronext.com>
2023-03-09 12:06:08 +0100

    exposing and deriving component props

--- src/components/labeled-checkbox.tsx
+++ src/components/labeled-checkbox.tsx
@@ -1,20 +1,21 @@
-import { ChangeEventHandler, FC, ReactNode, useId } from 'react';
+import { ChangeEventHandler, ComponentProps, FC, ReactNode, useId } from 'react';
 import { Checkbox } from './checkbox';
 import { Label } from './label';
 
-type Props = {
-  checked: boolean;
-  children: ReactNode;
-  onChange?: ChangeEventHandler;
-};
+/*
+  instead of listing all checkbox options in this component props to set them in parent component,
+  it gets the options from the Checkbox component and expose them(you don't need to know and specify all of them)
+  for instance disabled, checked, onChange and readonly opt for checkbox will be available to the parent component
+*/
+type Props = ComponentProps<typeof Checkbox>;
 
-export const LabeledCheckbox: FC<Props> = ({ checked, children, onChange }) => {
+export const LabeledCheckbox: FC<Props> = ({ children, id, ...props }) => {
   const internalId = useId();
 
   return (
     <div className="form-check">
-      <Checkbox checked={checked} id={internalId} onChange={onChange} />
-      <Label htmlFor={internalId}>{children}</Label>
+      <Checkbox id={id ?? internalId} {...props} />
+      <Label htmlFor={id ?? internalId}>{children}</Label>
     </div>
   );
 };
