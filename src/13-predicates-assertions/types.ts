e45bf0a3e76cedba6a76cde823122acc67b587cb
Federico Cao <fcao@euronext.com>
2023-03-14 22:28:28 +0100

    predicate functions for types

--- src/13-predicates-assertions/types.ts
+++ src/13-predicates-assertions/types.ts
@@ -4,14 +4,26 @@ export type Book = {
   description: string;
 };
 
+export function isBook(book: ItemsOnSale): book is Book {
+  return book.type === 'book';
+}
+
 export type Magazine = {
   type: string;
   title: string;
 };
 
+export function isMagazine(magazine: ItemsOnSale): magazine is Magazine {
+  return magazine.type === 'magazine';
+}
+
 export type Pen = {
   type: string;
   color: string;
 };
+
+export function isPen(pen: ItemsOnSale): pen is Pen {
+  return pen.type === 'pen';
+}
 
 export type ItemsOnSale = Book | Magazine | Pen;
