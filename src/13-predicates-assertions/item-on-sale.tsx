1727009d7097615d8067c864806de9c49c8d87db
Federico Cao <fcao@euronext.com>
2023-03-15 10:23:53 +0100

    type assertion functions

--- src/13-predicates-assertions/item-on-sale.tsx
+++ src/13-predicates-assertions/item-on-sale.tsx
@@ -2,21 +2,25 @@ import type { FC } from 'react';
 import { BookOnSale } from './book-on-sale';
 import { MagazineOnSale } from './magazine-on-sale';
 import { PenOnSale } from './pen-on-sale';
-import { Book, isBook, isMagazine, isPen, ItemsOnSale, Magazine, Pen } from './types';
+import { assertBook, assertMagazine, assertPen, Book, isBook, isMagazine, isPen, ItemsOnSale, Magazine, Pen } from './types';
 
 type Props = {
   item: ItemsOnSale;
 };
 
 export const ItemOnSale: FC<Props> = ({ item }) => {
-  if(isBook(item)) {
-    return <BookOnSale book={item} />;
-    
-  } else if(isMagazine(item)) {
-    return <MagazineOnSale magazine={item} />;
+  switch(item.type) {
+    case 'book':
+      assertBook(item);
+      return <BookOnSale book={item} />;
 
-  } else if(isPen(item)) {
-    return <PenOnSale pen={item} />;
+    case 'magazine':
+      assertMagazine(item);
+      return <MagazineOnSale magazine={item} />;
+
+    case 'pen':
+      assertPen(item);
+      return <PenOnSale pen={item} />;
   }
 
   return null;
--- src/13-predicates-assertions/types.ts
+++ src/13-predicates-assertions/types.ts
@@ -4,8 +4,14 @@ export type Book = {
   description: string;
 };
 
-export function isBook(book: ItemsOnSale): book is Book {
-  return book.type === 'book';
+export function isBook(item: ItemsOnSale): item is Book {
+  return item.type === 'book';
+}
+
+export function assertBook(item: ItemsOnSale): asserts item is Book {
+  if(!isBook(item)) {
+    throw new Error('Item is not a book');
+  }
 }
 
 export type Magazine = {
@@ -13,8 +19,14 @@ export type Magazine = {
   title: string;
 };
 
-export function isMagazine(magazine: ItemsOnSale): magazine is Magazine {
-  return magazine.type === 'magazine';
+export function isMagazine(item: ItemsOnSale): item is Magazine {
+  return item.type === 'magazine';
+}
+
+export function assertMagazine(item: ItemsOnSale): asserts item is Magazine {
+  if(!isMagazine(item)) {
+    throw new Error('Item is not a magazine');
+  }
 }
 
 export type Pen = {
@@ -22,8 +34,14 @@ export type Pen = {
   color: string;
 };
 
-export function isPen(pen: ItemsOnSale): pen is Pen {
-  return pen.type === 'pen';
+export function isPen(item: ItemsOnSale): item is Pen {
+  return item.type === 'pen';
+}
+
+export function assertPen(item: ItemsOnSale): asserts item is Pen {
+  if(!isPen(item)) {
+    throw new Error('Item is not a pen');
+  }
 }
 
 export type ItemsOnSale = Book | Magazine | Pen;
